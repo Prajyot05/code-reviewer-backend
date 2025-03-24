7678caa1891bf83f10333ef5ba05511ea76f47b9
Prajyot Tayde <pmtbmt@gmail.com>
2025-03-24 12:16:43 +0530

    change db to cloud

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,9 +4,11 @@ export const connectToDB = async () => {
   const MONGODB_URI = process.env.MONGODB_URI!;
 
   mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log(">>>>>>>>>Connected to MongoDB<<<<<<<<<");
+    .connect(MONGODB_URI, {
+      dbName: "code_reviewer",
+    })
+    .then((c) => {
+      console.log(`>>>>>>>>>DB connected to ${c.connection.host}<<<<<<<<<`);
     })
     .catch((err) => console.log("Error connecting to MongoDB: ", err));
 };
